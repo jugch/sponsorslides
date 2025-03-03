1bd7480cb9814c896d33b16a6dd77421963b697a
Nicolas Wanner <n.wanner@elderbyte.com>
2025-03-03 23:04:13 +0100

    fully rewrite slides

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JUG Sponsor Slides
 
-Configuration is done in js/config.js, all documentation is in there.
+Configuration is done in data.js
 
-This presentation is done with impress.js and relies on express.js as a webserver and less.js for styling.
-It currently supports one platinum sponsor, up to 10 gold sponsors and up to 40-50 silver sponsors.
+This presentation is done pure HTML, CSS and JS there are no external dependencies.
+It currently supports one platinum sponsor and many gold and silver sponsors.
--- data.js
+++ data.js
@@ -1,46 +1,14 @@
-/* exported config */
-/**
- * JUG Sponsor Slides
- *
- * @copyright 2014 und 2024 Java User Group Switzerland (jug.ch)
- */
-
-// Those are the general configuration settings for the JUG sponsor slides
-// Please do all your changes in here unless you're completely sure what you are doing!
-// SVGs work best for the sponsor logos
-// IMPORTANT: Do NOT remove any config options!
-var config = {
+const sponsorConfig = {
+    // Page title configuration
+    pageTitle: "JAVA USER GROUP CH",
+    
     // Content of the headline on the first slide
-    // Supported tags: <h1> <h2> <h3> <p> <i> <b>
-    // Set it to false to hide it completely
-    // headline: false,
     headline: '<h2><b>A big thanks to our Supporting Members</b></h2>',
-
-    // Slide display duration
-    // Time each slide is displayed in ms.
-    // Make sure that all animations fit in the duration
-    introDuration: 2000,
-    jugLogoDuration: 3000,
-
-    // Over all slides, each logo of the sponsors will be highlighted
-    // The following variables determine the times of these animations
-    // Duration: How long the highlight animation lasts
-    // Delay: The waiting time before the animation is being played
-
-    platinumDuration: 4000,
-    platinumDelay: 2000,
-
-    goldDuration: 3000,
-    goldDelay: 3500,
-
-    silverDuration: 2000,
-    silverDelay: 2250,
 
     // The platinum sponsor image, relative to the 'images' folder
     platinumSponsor: 'platinum/mimacom.png',
 
     // List of gold sponsor images, relative to the 'images' folder
-    // Update primarySponsorDuration if the number of sponsors is changed (4 Sponsors: 13000)
     goldSponsors: [
         'gold/bbv.svg',
         'gold/ergon.svg',
@@ -78,5 +46,24 @@ var config = {
         'silver/ti8m.png',
         'silver/worldline.png',
         'silver/zkb.png'
-    ]
-};
+    ],
+
+    // Slide display duration
+    // Time each slide is displayed in ms.
+    // Make sure that all animations fit in the duration
+    introDuration: 2000,
+    jugLogoDuration: 3000,
+
+    // Over all slides, each logo of the sponsors will be highlighted
+    // The following variables determine the times of these animations
+    // Duration: How long the highlight animation lasts
+    // Delay: The waiting time before the animation is being played
+    platinumDuration: 4000,
+    platinumDelay: 500,
+
+    goldDuration: 3500,
+    goldDelay: 500,
+
+    silverDuration: 3500,
+    silverDelay: 500
+}; 
